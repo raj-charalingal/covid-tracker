62e62eef090857f231f1ced359cb9630a0de24d7
raj-charalingal <charalingal.rajan@gmail.com>
2020-05-03 20:18:11 +0530

    started working on charts

--- src/api/api.index.js
+++ src/api/api.index.js
@@ -1,12 +1,24 @@
-import axios from 'axios';
+import axios from "axios";
 
-const url = 'https://covid19.mathdro.id/api';
-
+const url = "https://covid19.mathdro.id/api";
+const url2= "https://covid19.mathdro.id/api/daily";
 export const fetchData = async () => {
-    try {
-        const { data: { confirmed, recovered, deaths, lastUpdate }} = await axios.get(url);
-        return { confirmed, recovered, deaths, lastUpdate };
-    } catch(e) {
-      console.log(`an error occured. ${e}`)
-    }
-}
+  try {
+    const {
+      data: { confirmed, recovered, deaths, lastUpdate },
+    } = await axios.get(url);
+    return { confirmed, recovered, deaths, lastUpdate };
+  } catch (e) {
+    console.log(`an error occured while teching count. ${e}`);
+  }
+};
+
+export const fetchDailyData = async () => {
+  try {
+    const { data } = await axios.get(`${url}/daily`);
+    console.log(`================================`);
+    console.log(data);
+  } catch (e) {
+    console.log(`an error occured. ${e}`);
+  }
+};
--- src/components/CustomizeUI/Customize.js
+++ src/components/CustomizeUI/Customize.js
@@ -5,7 +5,7 @@ import { makeStyles } from '@material-ui/core/styles';
 
 export const useStyles =makeStyles((theme) => ({
     root: {
-      flexGrow: 1,
+      flexGrow: 2,
     }
     
     /* paper_typography1 : {
