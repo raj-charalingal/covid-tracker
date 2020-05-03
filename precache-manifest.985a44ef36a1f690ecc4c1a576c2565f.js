44070c4c1c4feb49452b5b9e7bfc8a0167fed169
raj-charalingal <charalingal.rajan@gmail.com>
2020-05-03 20:23:57 +0530

    Updates

---
+++ precache-manifest.985a44ef36a1f690ecc4c1a576c2565f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65aeb4541ddb70839831b65fc164da39",
+    "url": "/corona-tracker/index.html"
+  },
+  {
+    "revision": "eec672aef4f6ddbbe222",
+    "url": "/corona-tracker/static/css/main.8f07793c.chunk.css"
+  },
+  {
+    "revision": "ebfc974d831d524da795",
+    "url": "/corona-tracker/static/js/2.05215107.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/corona-tracker/static/js/2.05215107.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eec672aef4f6ddbbe222",
+    "url": "/corona-tracker/static/js/main.7be0a92f.chunk.js"
+  },
+  {
+    "revision": "4127b310accafee69bc6",
+    "url": "/corona-tracker/static/js/runtime-main.cfc71fe3.js"
+  }
+]);
