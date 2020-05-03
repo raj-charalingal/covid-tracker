e9903b631645b53f0a34c36cb28f912d7fbfc81a
raj-charalingal <charalingal.rajan@gmail.com>
2020-05-03 16:51:20 +0530

    second commit, styling cards

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,7 +1,9 @@
 import React from "react";
-import { Card, CardContent, Typography, Grid, Paper } from "@material-ui/core";
+import { Card, CardContent, Typography, Grid } from "@material-ui/core";
 import CountUp from "react-countup";
 import { useStyles } from '../CustomizeUI/Customize';
+import cx from 'classnames';
+import styles from './Cards.module.css';
 
 const Cards = ({ data: { confirmed, recovered, deaths, lastUpdate } }) => {
   const classes = useStyles();
@@ -9,55 +11,57 @@ const Cards = ({ data: { confirmed, recovered, deaths, lastUpdate } }) => {
     return "loading...";
   }
   return (
-    <div>
-      <Grid container spacing={3} justify="center" >
-        <Grid item xs={3}>
-          <Paper className={classes.paper}>
-              <Typography className={classes.paper_typography1} gutterBottom>Infected</Typography>
-              <Typography variant="h5">
-                <CountUp
-                  start={0}
-                  end={confirmed.value}
-                  duration={2.5}
-                  separator=","
-                />
-              </Typography>
-              <Typography className={classes.paper_typography2} >{new Date(lastUpdate).toDateString()}</Typography>
-              <Typography variant="body2">Active cases of COVID-19</Typography>
-          </Paper>
+    <div className={styles.container}>
+      <Grid container spacing={3} justify="center">
+        <Grid item xs={12} md={3} component={Card} className={cx(styles.card, styles.infected)}>
+          <CardContent>
+            <Typography className={styles.typographySub}>
+              Infected
+            </Typography>
+            <Typography variant="h5" component="h2" className={styles.typographyMain}>
+              <CountUp start={0} end={confirmed.value} duration={2.75} separator="," />
+            </Typography>
+            <Typography className={styles.typographySub}>
+              {new Date(lastUpdate).toDateString()}
+            </Typography>
+            <Typography variant="body2" component="p" className={styles.typographySub}>
+              Number of active cases of COVID-19.
+            </Typography>
+          </CardContent>
         </Grid>
-        <Grid item xs={3}>
-          <Paper className={classes.paper}>
-              <Typography className={classes.paper_typography1} gutterBottom>Recovered</Typography>
-              <Typography variant="h5">
-                <CountUp
-                  start={0}
-                  end={recovered.value}
-                  duration={2.5}
-                  separator=","
-                />
-              </Typography>
-              <Typography className={classes.paper_typography2} >{new Date(lastUpdate).toDateString()}</Typography>
-              <Typography variant="body2">Number of recovered cases of COVID-19</Typography>
-          </Paper>
+        <Grid item xs={12} md={3} component={Card} className={cx(styles.card, styles.recovered)}>
+          <CardContent>
+            <Typography className={styles.typographySub}>
+              Recovered
+            </Typography>
+            <Typography variant="h5" component="h2" className={styles.typographyMain}> 
+              <CountUp start={0} end={recovered.value} duration={2.75} separator="," />
+            </Typography>
+            <Typography className={styles.typographySub}>
+              {new Date(lastUpdate).toDateString()}
+            </Typography>
+            <Typography variant="body2" component="p" className={styles.typographySub}>
+              Number of recoveries from COVID-19.
+            </Typography>
+          </CardContent>
         </Grid>
-        <Grid item xs={3}>
-          <Paper className={classes.paper}>
-              <Typography className={classes.paper_typography1} gutterBottom>Deaths</Typography>
-              <Typography variant="h5">
-                <CountUp
-                  start={0}
-                  end={deaths.value}
-                  duration={2.5}
-                  separator=","
-                />
-              </Typography>
-              <Typography className={classes.paper_typography2} >{new Date(lastUpdate).toDateString()}</Typography>
-              <Typography variant="body2">Number of Deaths occured</Typography>
-          </Paper>
+        <Grid item xs={12} md={3} component={Card} className={cx(styles.card, styles.deaths)}>
+          <CardContent>
+            <Typography className={styles.typographySub}>
+              Deaths
+            </Typography>
+            <Typography variant="h5" component="h2" className={styles.typographyMain}>
+              <CountUp start={0} end={deaths.value} duration={2.75} separator="," />
+            </Typography>
+            <Typography className={styles.typographySub}>
+              {new Date(lastUpdate).toDateString()}
+            </Typography>
+            <Typography variant="body2" component="p" className={styles.typographySub}>
+              Number of deaths caused by COVID-19.
+            </Typography>
+          </CardContent>
         </Grid>
       </Grid>
-      
     </div>
   );
 };
--- src/components/CustomizeUI/Customize.js
+++ src/components/CustomizeUI/Customize.js
@@ -1,31 +1,13 @@
 import { withStyles, hexToRgb } from "@material-ui/core/styles";
 import { makeStyles } from '@material-ui/core/styles';
-import MuiExpansionPanel from "@material-ui/core/ExpansionPanel";
-import MuiExpansionPanelDetails from "@material-ui/core/ExpansionPanelDetails";
-import MuiExpansionPanelSummary from "@material-ui/core/ExpansionPanelSummary";
+
 
 
 export const useStyles =makeStyles((theme) => ({
     root: {
       flexGrow: 1,
-    },
-    container: {
-        margin: "5%",
-        display: "grid",
-        gridTemplateColumns: "repeat(12, 1fr)",
-        gridGap: `${theme.spacing.unit * 3}px`
-      },
-      paper: {
-        padding: theme.spacing(2),
-        textAlign: "center",
-        color: "rgb(255, 255, 255)",
-        background: "linear-gradient(147deg, #000000 0%, #434343 74%)",
-        whiteSpace: "nowrap",
-        marginBottom: theme.spacing.unit
-      } ,
-      divider: {
-        margin: `${theme.spacing.unit * 4}px 0`
-      }
+    }
+    
     /* paper_typography1 : {
         color: "rgb(255, 255, 255)"
     },
